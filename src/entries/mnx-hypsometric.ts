c5a213135e2fbbba1be139a9f975b5362006d171
mgermerie <73115044+mgermerie@users.noreply.github.com>
2025-03-10 19:10:28 +0100

    wip again

--- src/entries/mnx-hypsometric.ts
+++ src/entries/mnx-hypsometric.ts
@@ -6,7 +6,8 @@ import GeotiffParser from '../GeotiffParser';
 // ---------- CREATE THE VIEW: ----------
 
 const placement = {
-	coord: new itowns.Coordinates('EPSG:4326', 2.5, 42.5),
+	// coord: new itowns.Coordinates('EPSG:4326', 2.5, 42.5),
+	coord: new itowns.Coordinates('EPSG:4326', -1.5, 13.5),
 	range: 250000,
 };
 const viewContainer = document.getElementById('view-container');
@@ -15,26 +16,41 @@ const view = new itowns.GlobeView(viewContainer, placement);
 
 // ---------- DISPLAY A MAP: ----------
 
-function addMap() {
-	itowns.Fetcher.json('./resources/layers/OPENSM.json').then(function _(config) {
-		config.source = new itowns.TMSSource(config.source);
-		view.addLayer(
-			new itowns.ColorLayer('Map', config),
-		);
-	});
-}
+itowns.Fetcher.json('/resources/layers/OPENSM.json').then(function (config) {
+	config.source = new itowns.TMSSource(config.source);
+	const mapLayer = new itowns.ColorLayer('Map', config);
+
+	// view.addLayer(mapLayer);
+});
+
+
+// ---------- [TEST] TRY CUSTOM SHADER TO HIDE NO DATA: ----------
+
+itowns.ShaderChunk.customHeaderColorLayer(
+	`
+	const float toto=20.0;
+	const float blur = 0.8;
+	`,
+);
+
+itowns.ShaderChunk.customBodyColorLayer(
+	`
+	color = vec4(blur*color.xyz, 1.0);
+	`,
+);
 
 
 // ---------- DISPLAY MNX IN A ColorLayer: ----------
 
 const elevationSource = new itowns.WMSSource({
 	crs: 'EPSG:4326',
-	extent: new itowns.Extent('EPSG:4326', 1.999145, 3.000855, 41.999145, 43.000855),
-	name: 'SIS-test:E002N42_6EGTFLT32_MTSA1_001',
-	url: 'http://localhost:8080/geoserver/SIS-test/wms',
+	// extent: new itowns.Extent('EPSG:4326', 1.999145, 3.000855, 41.999145, 43.000855),
+	// url: 'http://localhost:8080/geoserver/SIS-test/wms',
+	// name: 'SIS-test:E002N42_6EGTFLT32_MTSA1_001',
+	extent: new itowns.Extent('EPSG:4326', -2.000021, -0.999979, 12.999979, 14.000021),
+	url: 'http://localhost:8080/geoserver/toto/wms',
+	name: 'toto:W002N13_4EGTFLT32_MTNE1_001',
 	version: '1.3.0',
-
-	transparent: true,
 
 	fetcher: Fetcher.geotiff,
 	parser: GeotiffParser.parse,
@@ -47,8 +63,20 @@ const elevationLayer = new itowns.ColorLayer(
 		source: elevationSource,
 		// effect_type: 1,
 		// TODO: try shader chunk to hide no data pixels
+		effect_type: itowns.colorLayerEffects.customEffect,
+		effect_parameter: 1.0,
 	},
 );
 
 view.addLayer(elevationLayer);
 
+
+// ---------- REORDER ColorLayers: ----------
+
+view.addEventListener(
+	itowns.GLOBE_VIEW_EVENTS.GLOBE_INITIALIZED,
+	() => {
+		itowns.ColorLayersOrdering.moveLayerToIndex(view, 'Map', 0);
+		console.log(view.getLayers());
+	},
+);
