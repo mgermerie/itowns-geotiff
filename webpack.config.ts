4e9552e2e063eab292fd3828c27ccb5916918c44
mgermerie <73115044+mgermerie@users.noreply.github.com>
2025-03-18 16:33:05 +0100

    add 3dtiles example

--- webpack.config.ts
+++ webpack.config.ts
@@ -14,6 +14,7 @@ const config = (env) => {
 		entry: {
 			'mnx-hypsometric': './src/entries/mnx-hypsometric.ts',
 			'ortho-mnx': './src/entries/ortho-mnx.ts',
+			'3d-tiles': './src/entries/3d-tiles.ts',
 		},
 
 		module: {
